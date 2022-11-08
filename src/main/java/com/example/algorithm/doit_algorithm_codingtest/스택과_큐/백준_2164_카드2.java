b337bcbc61f0d439e14ab83152d4c07194e12da9
gimminjae <min356812@naver.com>
2022-11-08 23:49:34 +0900

    백준_2164_카드2 : 코드 개선

--- src/main/java/com/example/algorithm/doit_algorithm_codingtest/스택과_큐/백준_2164_카드2.java
+++ src/main/java/com/example/algorithm/doit_algorithm_codingtest/스택과_큐/백준_2164_카드2.java
@@ -10,16 +10,9 @@ public class 백준_2164_카드2 {
             Queue<Integer> deck = new LinkedList<>();
             for(int i = 1; i <= N; i++) deck.offer(i);
 
-            //꺼낸 값을 버리는지,다시 넣는지 여부
-            boolean trash = true;
             while(deck.size() != 1) {
-                if(trash) {
                     deck.poll();
-                    trash = false;
-                } else {
                     deck.offer(deck.poll());
-                    trash = true;
-                }
             }
             System.out.println(deck.poll());
         } catch(Exception e) {
